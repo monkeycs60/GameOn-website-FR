749dcfacbb8d78d2c89cfd1bb28c016c3ef465f1
monkeycs60 <clement.serizay@gmail.com>
2022-06-29 03:23:43 +0200

    finition du formulaire première ébauche

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,6 +1,8 @@
 function editNav() {
-    let x = document.getElementById("myTopnav");
-    x.className === "topnav" ? x.className += " responsive" : x.className = "topnav";
+  let x = document.getElementById("myTopnav");
+  x.className === "topnav"
+    ? (x.className += " responsive")
+    : (x.className = "topnav");
 }
 
 // DOM Elements
@@ -9,14 +11,15 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModal = document.querySelector(".close");
 
-
 // const for the elements inside the form
 const prenom = document.querySelector("#first");
 const nom = document.querySelector("#last");
 const email = document.querySelector("#email");
 const birthdate = document.querySelector("#birthdate");
 const quantity = document.querySelector("#quantity");
-const cityForm = document.querySelector("cityForm");
+const cityForm = document.querySelector(".cityForm");
+const conditions = document.querySelector("#checkbox1");
+const validation = document.querySelector("#validation");
 
 //create new elements in the DOM
 const prenomErrorMessage = document.createElement("p");
@@ -33,7 +36,7 @@ closeModal.addEventListener("click", closeModalFunc);
 function launchModal() {
   modalbg.style.display = "block";
 }
-// close modal 
+// close modal
 function closeModalFunc() {
   modalbg.style.display = "none";
 }
@@ -46,7 +49,8 @@ prenom.addEventListener("keyup", validatePrenom);
 function validatePrenom() {
   if (prenom.value.length < 2) {
     prenom.style.border = "3px solid red";
-    prenomErrorMessage.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+    prenomErrorMessage.textContent =
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     //add error class
     prenomErrorMessage.classList.add("errorClass");
     prenom.parentElement.appendChild(prenomErrorMessage);
@@ -61,26 +65,28 @@ function validatePrenom() {
 //NOM
 nom.addEventListener("keyup", validateNom);
 
-  function validateNom() {
-    if (nom.value.length < 2) {
-      nom.style.border = "3px solid red";
-      nomErrorMessage.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-      //add error class
-      nomErrorMessage.classList.add("errorClass");
-      nom.parentElement.appendChild(nomErrorMessage);
-    } else {
-      nom.style.border = "3px solid green";
-      // errorMessage.textContent = "";
-      nomErrorMessage.classList.remove("errorClass");
-      nom.parentElement.removeChild(nomErrorMessage);
-    }
+function validateNom() {
+  if (nom.value.length < 2) {
+    nom.style.border = "3px solid red";
+    nomErrorMessage.textContent =
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    //add error class
+    nomErrorMessage.classList.add("errorClass");
+    nom.parentElement.appendChild(nomErrorMessage);
+  } else {
+    nom.style.border = "3px solid green";
+    // errorMessage.textContent = "";
+    nomErrorMessage.classList.remove("errorClass");
+    nom.parentElement.removeChild(nomErrorMessage);
   }
+}
 
 //EMAIL
 email.addEventListener("keyup", validateEmail);
 
-  function validateEmail() {
-  const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function validateEmail() {
+  const emailRegExp =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (!emailRegExp.test(email.value)) {
     email.style.border = "3px solid red";
@@ -88,8 +94,7 @@ email.addEventListener("keyup", validateEmail);
     //add error class
     emailErrorMessage.classList.add("errorClass");
     email.parentElement.appendChild(emailErrorMessage);
-  }
-  else {
+  } else {
     email.style.border = "3px solid green";
     // errorMessage.textContent = "";
     emailErrorMessage.classList.remove("errorClass");
@@ -97,78 +102,106 @@ email.addEventListener("keyup", validateEmail);
   }
 }
 
- // QUANTITY (e) => console.log(e.target.value)
- //adevent listener if you change the value with the button at the right of the input
+//BIRTHDATE
+
+birthdate.addEventListener("change", validateBirthdate);
+
+function validateBirthdate() {
+  const birthdateRegExp =
+    /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+  const date = new Date(birthdate.value);
+  const dateString = date.toLocaleDateString();
+
+  if (!birthdateRegExp.test(dateString)) {
+    birthdate.style.border = "3px solid red";
+    birthdateErrorMessage.textContent =
+      "Veuillez entrer une date de naissance valide.";
+    //add error class
+    birthdateErrorMessage.classList.add("errorClass");
+    birthdate.parentElement.appendChild(birthdateErrorMessage);
+  }
+  //if e.target.value is undefined or <0, then error message
+  else {
+    birthdate.style.border = "3px solid green";
+    // errorMessage.textContent = "";
+    birthdateErrorMessage.classList.remove("errorClass");
+    birthdate.parentElement.removeChild(birthdateErrorMessage);
+  }
+}
+
+// QUANTITY (e) => console.log(e.target.value)
+//adevent listener if you change the value with the button at the right of the input
 quantity.addEventListener("change", validateUpDown);
 
-  function validateUpDown() {
-    //if e.target.value is >=0, then success message
-    if (quantity.value >= 0) {
-      quantity.style.border = "3px solid green";
-      // errorMessage.textContent = "";
-      quantityErrorMessage.classList.remove("errorClass");
-      quantity.parentElement.removeChild(quantityErrorMessage);
-    }
-    //if e.target.value is undefined or <0, then error message
-    else {
-      quantity.style.border = "3px solid red";
-      quantityErrorMessage.textContent = "Veuillez entrer une quantité positive.";
-      //add error class
-      quantityErrorMessage.classList.add("errorClass");
-      quantity.parentElement.appendChild(quantityErrorMessage);
-    }
+function validateUpDown() {
+  //if e.target.value is >=0, then success message
+  if (quantity.value >= 0) {
+    quantity.style.border = "3px solid green";
+    // errorMessage.textContent = "";
+    quantityErrorMessage.classList.remove("errorClass");
+    quantity.parentElement.removeChild(quantityErrorMessage);
   }
-
-
+  //if e.target.value is undefined or <0, then error message
+  else {
+    quantity.style.border = "3px solid red";
+    quantityErrorMessage.textContent = "Veuillez entrer une quantité positive.";
+    //add error class
+    quantityErrorMessage.classList.add("errorClass");
+    quantity.parentElement.appendChild(quantityErrorMessage);
+  }
+}
 
 //event listener if you press a key inside the quantity input
 quantity.addEventListener("keyup", validateQuantity);
 
-  function validateQuantity() {
-    //QUANTITY value has to be a number
-    if (isNaN(quantity.value)) {
-      quantity.style.border = "3px solid red";
-      quantityErrorMessage.textContent = "Veuillez entrer un nombre pour le champ de la quantité.";
-      //add error class
-      quantityErrorMessage.classList.add("errorClass");
-      quantity.parentElement.appendChild(quantityErrorMessage);
-    }
-    //else if quantity value is empty
-    else if (quantity.value === "") {
-      quantity.style.border = "3px solid red";
-      quantityErrorMessage.textContent = "Veuillez entrer une quantité pour le champ de la quantité.";
-      //add error class
-      quantityErrorMessage.classList.add("errorClass");
-      quantity.parentElement.appendChild(quantityErrorMessage);
-    }
-    else {
-      quantity.style.border = "3px solid green";
-      // errorMessage.textContent = "";
-      quantityErrorMessage.classList.remove("errorClass");
-      quantity.parentElement.removeChild(quantityErrorMessage);
-    }
+function validateQuantity() {
+  //QUANTITY value has to be a number
+  if (isNaN(quantity.value)) {
+    quantity.style.border = "3px solid red";
+    quantityErrorMessage.textContent =
+      "Veuillez entrer un nombre pour le champ de la quantité.";
+    //add error class
+    quantityErrorMessage.classList.add("errorClass");
+    quantity.parentElement.appendChild(quantityErrorMessage);
+  }
+  //else if quantity value is empty
+  else if (quantity.value === "") {
+    quantity.style.border = "3px solid red";
+    quantityErrorMessage.textContent =
+      "Veuillez entrer une quantité pour le champ de la quantité.";
+    //add error class
+    quantityErrorMessage.classList.add("errorClass");
+    quantity.parentElement.appendChild(quantityErrorMessage);
+  } else {
+    quantity.style.border = "3px solid green";
+    // errorMessage.textContent = "";
+    quantityErrorMessage.classList.remove("errorClass");
+    quantity.parentElement.removeChild(quantityErrorMessage);
+  }
+}
+
+//SUBMIT BUTTON
+validation.addEventListener("click", (e) => {
+  e.preventDefault();
+  validateForm();
+});
+
+function validateForm() {
+  //if all borders are green, then submit the form
+
+  let counter = 0;
+  if (
+    prenom.style.border === "3px solid green" &&
+    nom.style.border === "3px solid green" &&
+    email.style.border === "3px solid green" &&
+    birthdate.style.border === "3px solid green" &&
+    quantity.style.border === "3px solid green" &&
+    document.querySelector("input[name='location']:checked") !== null &&
+    document.querySelector("input[id='checkbox1']:checked") !== null
+  ) {
+    counter++;
+    console.log(counter);
   }
 
-//error message if none of the radio button are checked in the city form
-// cityForm.addEventListener("click", validateCity);
-
-//   function validateCity() {
-//     if (!cityForm.checked) {
-//       cityForm.style.border = "3px solid red";
-//       cityErrorMessage.textContent = "Veuillez choisir une ville pour le champ de la ville.";
-//       //add error class
-//       cityErrorMessage.classList.add("errorClass");
-//       cityForm.parentElement.appendChild(cityErrorMessage);
-//     }
-//     else {
-//       cityForm.style.border = "3px solid green";
-//       // errorMessage.textContent = "";
-//       cityErrorMessage.classList.remove("errorClass");
-//       cityForm.parentElement.removeChild(cityErrorMessage);
-//     }
-//   }
-  
-
-  
-  
  
+}
